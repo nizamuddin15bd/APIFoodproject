e88e3231a109f66639c330afb996754ec0464354
nizamuddin15bd <nizamuddin15bd@gmail.com>
2022-11-05 19:37:23 +0600

    first commit

---
+++ buddy.js
@@ -0,0 +1,20 @@
+const loadBuddies = () => {
+    fetch('https://randomuser.me/api/?results=5')
+        .then(res => res.json())
+        .then(data => displayBuddies(data))
+}
+loadBuddies();
+
+const displayBuddies = data => {
+    const buddies = data.results;
+    const buddiesDiv = document.getElementById('buddies')
+    // console.log(buddies);
+    for (const buddy of buddies) {
+        console.log(buddy)
+        const p = document.createElement('p');
+        p.innerText = `
+        first Name: ${buddy.name.last}.last Name ${buddy.name.last}. phone Number: ${buddy.phone}. Email: ${buddy.email}
+        `;
+        buddiesDiv.appendChild(p);
+    }
+}
---
+++ problem.js
@@ -0,0 +1,7 @@
+const first = 60;
+const second = 80;
+const third = 81;
+
+if (first > 60) {
+
+}
